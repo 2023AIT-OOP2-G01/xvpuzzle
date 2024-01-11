bcf46ecb69be936233c26dc01e2a6d5929f8a9f8
赤部翔利 <k22075kk@aitech.ac.jp>
2024-01-12 02:31:50 +0900

    分割された画像をstaticフォルダに送る

--- diviededimage_send.py
+++ diviededimage_send.py
@@ -1,2 +1,26 @@
+# dividedimage_send.py
 #分割した画像をstaticの中に送る部分を作ります
-pass
+
+import os
+from shutil import copyfile
+
+def send_divided_images():
+    # 分割した画像が保存されているディレクトリ
+    segments_folder = 'segments'
+
+    # staticフォルダに分割した画像をコピー
+    for file_name in os.listdir(segments_folder):
+        # 分割された画像のパス
+        source_path = os.path.join(segments_folder, file_name)
+        
+        # staticフォルダにコピーする先のパス
+        destination_path = os.path.join('static', file_name)
+
+        # 画像をstaticフォルダにコピー
+        copyfile(source_path, destination_path)
+
+    print('Divided images sent to static folder')
+
+if __name__ == '__main__':
+    # メインとして実行された場合、画像をstaticフォルダに送る
+    send_divided_images()
